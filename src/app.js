e5f3cc6b532ced58ee3cbf33e6b8e9bcbc848029
Arjanne <79474276+Arjanne@users.noreply.github.com>
2021-04-10 18:14:48 +0200

    Some styling

--- src/app.js
+++ src/app.js
@@ -40,13 +40,23 @@ function displayTemperature(response) {
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
-
-    console.log(response.data);
 }
 
-let apiKey = "3ef3d8132df153f5933e2cde2a4d420d";
-let city = "Amsterdam";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "3ef3d8132df153f5933e2cde2a4d420d";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
 
-axios.get(apiUrl).then(displayTemperature);
+function submitCity(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", submitCity)
+
+search("Amsterdam");
